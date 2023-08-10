9f64dca664c36a072f1a62a07bb4d7d5445be50c
B4g4rini <pedro.bagarini2000@gmail.com>
2023-08-10 09:41:46 -0300

    adicionado a troca de imagem, ajuste na imagem

--- js/script.js
+++ js/script.js
@@ -3,4 +3,13 @@ var toggleMode = () => {
 
     html.classList.toggle("light");
 
+    const isLight = html.classList.contains("light");
+    const srcValue = isLight ? "../img/avatar-light.jpg" : "../img/avatar.jpg";
+    const altValue = isLight ? "Imagem de perfil no modo claro, com um jovem de cabelo curto, segurando um celular, vestindo camiseta escura" : "Imagem de perfil no modo escuro, com um jovem de cabelo curto segurando o celular, vestindo uma camiseta escura estampada com a bandeira do Brasil "
+
+
+
+    document.querySelector("#profile img").setAttribute("src",srcValue);
+    document.querySelector("#profile img").setAttribute("alt", altValue);
+
 };
