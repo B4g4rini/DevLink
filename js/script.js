cf912789e456efbcfe404e5042c5c98d04df8371
B4g4rini <pedro.bagarini2000@gmail.com>
2023-08-10 11:21:13 -0300

    Foto nova adicionada

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var toggleMode = () => {
 
     const isLight = html.classList.contains("light");
     const srcValue = isLight ? "../img/avatar-light.jpg" : "../img/avatar.jpg";
-    const altValue = isLight ? "Imagem de perfil no modo claro, com um jovem de cabelo curto, segurando um celular, vestindo camiseta escura" : "Imagem de perfil no modo escuro, com um jovem de cabelo curto segurando o celular, vestindo uma camiseta escura estampada com a bandeira do Brasil "
+    const altValue = isLight ? "Imagem de perfil no modo claro, com um jovem de cabelo curto, segurando um celular, vestindo camiseta escura e de corrente no pescoço" : "Imagem de perfil no modo escuro, com um jovem de cabelo curto segurando o celular, vestindo uma camiseta escura estampada com a bandeira do Brasil "
 
 
 
